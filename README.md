529393deafe05d4c23c2d82d70821ecb4dff6687
gouthamjp <45317531+gouthamjp@users.noreply.github.com>
2021-10-21 18:49:10 +0530

    linkedlist 1

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 This repo contains some fun programming questions and links to certain techniques  
  
 ## Links
-[Window Sliding Technique](https://www.geeksforgeeks.org/window-sliding-technique/)
+[Window Sliding Technique](https://www.geeksforgeeks.org/window-sliding-technique/)  
 [Two Pointer Technique](https://www.geeksforgeeks.org/two-pointers-technique/)
---
+++ reverseLinked.py
@@ -0,0 +1,20 @@
+#shttps://leetcode.com/problems/linked-list-cycle/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        cur = head;
+        prev = None;
+        while(cur):
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            
+            cur = temp
+        
+        return prev
+            
